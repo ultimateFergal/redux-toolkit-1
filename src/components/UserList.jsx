75343f336c19ec9bdbf401b306dc13b93a7ae3f2
fernandoGalarcio <fdowannabe@gmail.com>
2023-04-28 22:39:50 -0500

    redux tooklkit for fething data

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -11,26 +11,27 @@ const UserList = () => {
     useEffect(() => {
         dispatch(fetchAllUsers());
     }, [dispatch])
-  return (
-    <div className='container mt-4'>
-        <div className='row'>
-            {
-                list.map((user, index) => (
-                    <div key={index} className='col-md-3 mb-4'>
-                        <div className='card'>
-                            <img src={user.avatar} alt="avatar" />
-                            <div className='card-body'>
-                                <h5 className='card-title'>{`${user.first_name} ${user.last_name}`}</h5>
-                                <p className='card-text'>{user.email}</p>
-                            </div>
+    
+    return (
+        <div className='container mt-4'>
+            <div className='row'>
+                {
+                    list.map((user, index) => (
+                        <div key={index} className='col-md-3 mb-4'>
+                            <div className='card'>
+                                <img src={user.avatar} alt="avatar" />
+                                <div className='card-body'>
+                                    <h5 className='card-title'>{`${user.first_name} ${user.last_name}`}</h5>
+                                    <p className='card-text'>{user.email}</p>
+                                </div>
 
+                            </div>
                         </div>
-                    </div>
-                ))
-            }
+                    ))
+                }
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default UserList;
